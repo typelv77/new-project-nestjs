b30f94c1a6e66199fe6c295c6eaa613e54cf71fc
typelv77 <typelvsenai@gmail.com>
2023-10-09 21:47:57 -0300

    doc: example creat dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,19 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MinLength } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({ example: "Lunna"})
     @IsString()
     name: string;
 
+    @ApiProperty({ example: "email.com"})
     @IsEmail()
     email: string;
 
+    @ApiProperty({ example: "@123hffg"})
     @IsString()
     @MinLength(8, { message: 'Senha deve conter 8 digitos' })
     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'Senha muito fraca',
   })
     password: string;
-
+    @ApiProperty({ example: "link"})
     @IsString()
     picture: string;
 }
