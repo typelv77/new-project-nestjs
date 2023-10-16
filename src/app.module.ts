21ea42e01811dea0cdd15a628d27197e733a93b8
typelv77 <typelvsenai@gmail.com>
2023-10-16 20:44:38 -0300

    add module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
