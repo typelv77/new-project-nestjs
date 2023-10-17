4d6bc72c2cf6b5e46594fb1128490b8b9ab24e84
typelv77 <typelvsenai@gmail.com>
2023-10-16 21:45:36 -0300

    feat:  add local.strategy.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,33 @@
+// import { Module } from '@nestjs/common';
+// import { PrismaModule } from 'src/prisma/prisma.module';
+// import { AuthService } from './auth.service';
+
+// @Module({
+//     imports: [PrismaModule],
+//     providers: [AuthService],
+
+// })
+// export class AuthModule {}
+
+
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { UserModule } from 'src/user/user.module';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-    imports: [PrismaModule],
-    providers: [AuthService],
-
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,38 @@
+// import { Injectable } from '@nestjs/common';
+// import { User } from '@prisma/client';
+// import { UserService } from 'src/user/user.service';
+
+// @Injectable()
+// export class AuthService {
+//     constructor(private usersService: UserService) {}
+
+//   async validateUser(email: string, password: string): Promise< {
+//     id: string;
+//     email: string;
+//     name: string;
+//     picture: string;
+//     admin: boolean;
+// }| null > {
+//     const user: User = await this.usersService.findOne(email);
+//     if (user && user.password === password) {
+//       const { password, ...result } = user;
+//       return result;
+//     }
+//     throw Error("Senha incorreta!");
+//   }
+// }
+
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService
+  ) {}
 
   async validateUser(email: string, password: string): Promise< {
     id: string;
@@ -19,5 +47,12 @@ export class AuthService {
       return result;
     }
     throw Error("Senha incorreta!");
+  }
+
+  async login(user: User) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
